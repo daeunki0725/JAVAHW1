2522ef35bcac3747e44ada8b7ed200db5b63101f
Daeun Kim <76934380+daeunki0725@users.noreply.github.com>
2021-05-02 13:34:18 -0700

    Update keyExample.java

--- keyExample.java
+++ keyExample.java
@@ -6,6 +6,7 @@ import java.awt.geom.*;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
+import java.time.ZonedDateTime;
 
 public class keyExample extends JPanel implements ActionListener, KeyListener {
 
@@ -14,7 +15,10 @@ public class keyExample extends JPanel implements ActionListener, KeyListener {
   double changeX = 0, changeY = 0;
   int f = 0;
   int[][] values = new int[7][6];
-
+  int lives = 3;
+  long time = ZonedDateTime.now().toInstant().toEpochMilli();
+  boolean gameover = false;
+  
   public static void main(String args[]) {
     JFrame theGUI = new JFrame();
     keyExample s = new keyExample();
@@ -47,18 +51,41 @@ public class keyExample extends JPanel implements ActionListener, KeyListener {
   
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
-
+    	
     Graphics2D g2 = (Graphics2D) g;
     g2.setColor(Color.BLUE);
     g2.fill(new Rectangle2D.Double(x, y, 40, 40));
-
-    Rectangle box1 = new Rectangle(5, 10, 20, 30);
-    Rectangle box2 = new Rectangle(25, 10, 20, 30);
-    Rectangle box3 = new Rectangle(45, 10, 20, 30);
     
-    g2.draw(box1);
-    g2.draw(box2);
-    g2.draw(box3);
+    if (gameover == true) {
+      
+    g2.drawString("GAME OVER LOSER", 200, 200);
+    
+    if (ZonedDateTime.now().toInstant().toEpochMilli() - time > 3000) 
+    
+    System.exit(0);
+    }
+    
+    if (checkHit() && ZonedDateTime.now().toInstant().toEpochMilli() - time > 5000) { 
+    	System.out.println("HIT");
+    	
+    	lives--;
+    	
+    	time = ZonedDateTime.now().toInstant().toEpochMilli();
+    	
+    	if (lives == 0){
+    	
+    	 gameover = true;
+
+    	}
+    	}
+    
+    for (int i = 0; i < lives; i++) {
+		
+    Rectangle box = new Rectangle(i*20+5, 10, 20, 30);
+    
+    g2.draw(box);
+    
+    }
     
     double x;
 
@@ -71,7 +98,20 @@ public class keyExample extends JPanel implements ActionListener, KeyListener {
         if (values[i][1] >= 400) setValues(i, i + 1); 
     }
   }
-
+	private boolean checkHit() { // function to check hit - you have write this!
+	
+	for (int i = 0; i < 7; i++) {
+		double y1 = i / 7*600;
+		double x1 = values[i][1];
+		
+		double distance = Math.pow(Math.pow(x1 - x, 2) + Math.pow(y1 - y, 2), 1/2);
+		double threshold = 20 + (double)values[i][0]/2;
+		
+		if (distance < threshold)
+		return true;
+		}
+		return false;
+	}
   public void actionPerformed(ActionEvent e) {
     Scanner sc = new Scanner(System.in);
     repaint();
